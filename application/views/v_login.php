9a26c028d7ee4b8d371058f6c46fb0a0e414902f
andrianmy10 <andrian5yusup@gmail.com>
2026-04-20 23:02:29 +0700

    bug fixing

--- application/views/v_login.php
+++ application/views/v_login.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-    <title>Portal Yayasan Asih Putera</title>
+    <title>Mini Portal - YAP</title>
+<link rel="icon" type="image/png" href="<?= base_url('assets/images/icon.png') ?>" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
